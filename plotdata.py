9a65b57b27e1e02dcd6ed2910c440f5c92e7e6f3
Jingzc <cjz971027@126.com>
2019-05-16 13:21:03 +0800

    更新

--- plotdata.py
+++ plotdata.py
@@ -10,22 +10,6 @@ def loaddata(path):
         lst=list(data.iloc[i])
         datapre.append(lst)
     return datapre
-def floatrange(start,stop,steps):
-    ''' Computes a range of floating value.
-
-        Input:
-            start (float)  : Start value.
-            end   (float)  : End value
-            steps (integer): Number of values
-
-        Output:
-            A list of floats
-
-        Example:
-            >>> print floatrange(0.25, 1.3, 5)
-            [0.25, 0.51249999999999996, 0.77500000000000002, 1.0375000000000001, 1.3]
-    '''
-    return [start+float(i)*(stop-start)/(float(steps)-1) for i in range(steps)]
 
 if __name__=='__main__':
     path=(r'data\ratings.csv')
